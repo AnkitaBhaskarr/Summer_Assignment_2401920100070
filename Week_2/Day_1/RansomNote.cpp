176cdae027d7d2237f6b37209e0f5ad6e177d2e4
AnkitaBhaskarr <ankitabhaskarr@gmail.com>
2026-06-08 23:37:49 +0530

    Create RansomNote.cpp

---
+++ Week_2/Day_1/RansomNote.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        vector<int> freq(26, 0);
+
+        for (char c : magazine) {
+            freq[c - 'a']++;
+        }
+
+        for (char c : ransomNote) {
+            freq[c - 'a']--;
+
+            if (freq[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
