25d4ab2343df80baedf099524e461c6df31eea4f
AnkitaBhaskarr <ankitabhaskarr@gmail.com>
2026-06-11 22:30:03 +0530

    Create DecodeString.cpp

---
+++ Week_2/Day_4/DecodeString.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    string decodeString(string s) {
+
+        stack<int> countStack;
+        stack<string> stringStack;
+
+        string currentString = "";
+        int k = 0;
+
+        for (char ch : s) {
+
+            if (isdigit(ch)) {
+                k = k * 10 + (ch - '0');
+            }
+            else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+
+                k = 0;
+                currentString = "";
+            }
+            else if (ch == ']') {
+
+                string decoded = stringStack.top();
+                stringStack.pop();
+
+                int repeat = countStack.top();
+                countStack.pop();
+
+                for (int i = 0; i < repeat; i++) {
+                    decoded += currentString;
+                }
+
+                currentString = decoded;
+            }
+            else {
+                currentString += ch;
+            }
+        }
+
+        return currentString;
+    }
+};
