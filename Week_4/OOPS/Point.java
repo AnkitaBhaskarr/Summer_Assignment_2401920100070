7e582ce702a33f6d6b7050121e124f6d6bf4c48c
AnkitaBhaskarr <ankitabhaskarr@gmail.com>
2026-06-27 11:36:17 +0530

    Create Point.java

---
+++ Week_4/OOPS/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+    private int x, y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void display() {
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point();
+        p1.setXY(10, 20);
+        p1.display();
+
+        Point p2 = new Point(30, 40);
+        p2.display();
+    }
+}
